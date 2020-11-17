e90bc1b8a45b814be6dec936c88de8c6e90043fe
jpoou <juancarlosmorales85@gmail.com>
2020-11-17 13:58:53 -0600

    add emiter event ss

--- dw-api/src/main/java/com/umg/dw/api/DWAPIApplication.java
+++ dw-api/src/main/java/com/umg/dw/api/DWAPIApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -19,6 +20,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EntityScan(basePackages = {"com.umg.dw.core.entities"})
 @EnableJpaRepositories(basePackages = {"com.umg.dw.api"})
 @ComponentScan(basePackages = {"com.umg.dw"})
+@EnableEurekaClient
 public class DWAPIApplication {
 
     public static void main(String[] args) {
--- dw-api/src/main/java/com/umg/dw/api/controller/NoteController.java
+++ dw-api/src/main/java/com/umg/dw/api/controller/NoteController.java
@@ -1,14 +1,17 @@
 package com.umg.dw.api.controller;
 
-import com.umg.dw.api.exception.ResourceNotFoundException;
 import com.umg.dw.api.repository.NoteRepository;
 import com.umg.dw.core.entities.Note;
 import io.swagger.annotations.Api;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.EmitterProcessor;
+import reactor.core.publisher.Flux;
 
+import javax.annotation.PostConstruct;
+import java.time.Duration;
 import java.util.List;
-import java.util.Optional;
+import java.util.UUID;
 
 @Api(value = "/", description = "REST note")
 @RestController
@@ -17,9 +20,15 @@ public class NoteController {
 
     final
     NoteRepository noteRepository;
+    private EmitterProcessor<Note> notificationProcessor;
 
     public NoteController(NoteRepository noteRepository) {
         this.noteRepository = noteRepository;
+    }
+
+    @PostConstruct
+    private void createProcessor() {
+        notificationProcessor = EmitterProcessor.<Note>create();
     }
 
     @CrossOrigin
@@ -45,14 +54,51 @@ public class NoteController {
             produces = "application/json")
     public Note create(@RequestBody Note note) {
         note = noteRepository.save(note);
+        notificationProcessor.onNext(note);
         return note;
     }
+
     @RequestMapping(
             method = RequestMethod.PUT,
             produces = "application/json")
     public Note update(@RequestBody Note note) {
         note = noteRepository.save(note);
         return note;
+    }
+
+    private Flux<ServerSentEvent<Note>> getPersonaSSE() {
+
+        return notificationProcessor
+            .log().map(
+                (note) -> {
+                    System.out.println("Sending Note:" + note.getBody());
+                    return ServerSentEvent.<Note>builder()
+                            .id(UUID.randomUUID().toString())
+                            .event("note-result")
+                            .data(note)
+                            .build();
+                }).concatWith(Flux.never());
+    }
+
+    private Flux<ServerSentEvent<Note>> getNotificationHeartbeat() {
+        return Flux.interval(Duration.ofSeconds(15))
+            .map(i -> {
+                System.out.println(String.format("Sending heartbeat [%s] ...", i.toString()));
+                return ServerSentEvent.<Note>builder()
+                        .id(String.valueOf(i))
+                        .event("heartbeat-result")
+                        .data(null)
+                        .build();
+            });
+    }
+
+    @GetMapping(
+            value = "/notification/sse"
+    )
+    public Flux<ServerSentEvent<Note>> getJobResultNotification() {
+
+        return Flux.merge(getNotificationHeartbeat(), getPersonaSSE());
+
     }
 
 }
